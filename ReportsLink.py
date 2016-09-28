fe099d3eac7ff831b3d6d3519cf0567a4d0adaa6
shobhak <shobhak@gmail.com>
2016-09-28 15:35:30 -0700

    Add files via upload

--- ReportsLink.py
+++ ReportsLink.py
@@ -13,10 +13,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class WvLogin(unittest.TestCase):
     def setUp(self):
+        #chrome browser
         self. driver = webdriver.Chrome('C:\Users\shobha\chromedriver')
+        # if using firefox take the comment out and comment the previous statement.
         #self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(2)
         self.wait = WebDriverWait(self.driver,2)
+        #url to login
         self.base_url = "http://testwave.qabidder.net/#/page/login"
         time.sleep(1)
 
@@ -25,22 +28,28 @@ class WvLogin(unittest.TestCase):
         driver.get(self.base_url)
         wait = WebDriverWait(driver,2)
         time.sleep(3)
-        driver.find_element_by_id("exampleInputEmail1").click()
+        #email field
         driver.find_element_by_xpath("//input[@id='exampleInputEmail1']").clear()
         driver.find_element_by_xpath("//input").send_keys("dummyemail1938+4@gmail.com")
+        #password field
         driver.find_element_by_xpath(".//*[@id='exampleInputPassword1']").clear()
         driver.find_element_by_xpath(".//*[@id='exampleInputPassword1']").send_keys("Thisis4u")
+        #wait
         time.sleep(3)
+        # submit
         driver.find_element_by_xpath("//button").click()
+
 
         # passed Navigation test case #2 Verify that the reports button in main navigation works
         driver.find_element_by_xpath("//a/span").is_selected()
 
         # wait
         time.sleep(2)
+        # +++ testing logout  functionality
+        driver.find_element_by_link_text('Logout').click()
+        time.sleep(2)
 
         # shut it down
-
     def tearDown(self):
         self.driver.quit()
 
