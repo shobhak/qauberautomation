5c818ec400efb43828980774392c97c8cf0014ee
Arkadiy-Plekhanov <arkadiy.plekhanov@gmail.com>
2016-09-28 11:55:29 -0700

    Add files via upload
    Selenium-Webdriver 2.53.0, Ruby 2.2.5

---
+++ T76674.rb
@@ -0,0 +1,34 @@
+#importing WebDriver
+require "selenium-webdriver";
+
+#Specifying Browser for Webdriver
+driver = Selenium::WebDriver.for :chrome;
+
+#TC: Verify "Entities" link is functional https://bidqa.testrail.net/index.php?/tests/view/76674
+
+#navigate to login page
+driver.get("http://testwave.qabidder.net/#/page/login");
+sleep(3)
+
+#login
+driver.find_element(:xpath, "//input[@id='exampleInputEmail1']").send_keys("arkadiy.plekhanov@yahoo.com");
+driver.find_element(:xpath, "//input[@id='exampleInputPassword1']").send_keys("qwerty123");
+driver.find_element(:xpath, "//button[@type='submit']").click;
+sleep(3)
+
+#Checking if link is functional
+driver.find_element(:xpath, "//span[contains(.,'Entities')]").click
+sleep(2)
+
+#Veryfying if "Entities" page opens.
+if (driver.find_element(:xpath, "//button[contains(.,'Add Organization')]")).displayed?
+  puts "Passed"
+else
+  puts "Failed"
+end
+
+#clearing cache
+driver.manage.delete_all_cookies;
+
+#closing Browser
+driver.quit;
