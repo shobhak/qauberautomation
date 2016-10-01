893986d7c4def0c627e258628286288544f9c492
avanipatel11 <avaniptl@gmail.com>
2016-10-01 10:03:05 -0700

    Update T77291_Random_SuperAdmin_AddReport_Preview.py

--- T77291_Random_SuperAdmin_AddReport_Preview.py
+++ T77291_Random_SuperAdmin_AddReport_Preview.py
@@ -5,8 +5,8 @@ import unittest
 
 class Addreport_Preview(unittest.TestCase):
     def setUp(self):
-        #self.driver = webdriver.Chrome()
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
+        #self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "http://www.testwave.qabidder.net/"
 
@@ -25,29 +25,40 @@ class Addreport_Preview(unittest.TestCase):
         driver.find_element_by_xpath("//button[@type='submit']").click()
         #Go to add report page
         driver.find_element_by_xpath("//span[contains(.,'Add Report')]").click()
+        #Select department name
         driver.find_element_by_xpath("//label[contains(.,'Neoaccel')]").click()
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[2]/ul/li/a").click()
         time.sleep(2)
+        #enter  random first name
         driver.find_element_by_xpath("//input[@name='first-name']").clear()
         driver.find_element_by_xpath("//input[@name='first-name']").send_keys(first_name)
         print(first_name)
         time.sleep(2)
+        #enter  random last name
         driver.find_element_by_xpath("//input[@name='last-name']").clear()
         driver.find_element_by_xpath("//input[@name='last-name']").send_keys(last_name)
         print(last_name)
         time.sleep(2)
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[3]/ul/li[2]/a").click()
         time.sleep(2)
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[4]/ul/li[2]/a").click()
         time.sleep(2)
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[5]/ul/li[2]/a").click()
         time.sleep(2)
+        # enter Stop Location
         driver.find_element_by_xpath("//input[@placeholder = 'Stop Location']").clear()
         driver.find_element_by_xpath("//input[@placeholder = 'Stop Location']").send_keys("Sunnyvale")
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[6]/ul/li[2]/a").click()
         time.sleep(2)
+        #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[7]/ul/li[2]/a").click()
         time.sleep(2)
+         #Go to next step
         driver.find_element_by_xpath("//div[2]/section/div/div/div/form/div/div[8]/ul/li[2]/a").click()
         time.sleep(2)
         print("You are on preview page")
