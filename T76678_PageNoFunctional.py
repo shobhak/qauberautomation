a02dbeae704c97a10d1aa3335a5b4616d8fe44ba
avanipatel11 <avaniptl@gmail.com>
2016-10-01 09:05:37 -0700

    Add files via upload

--- T76678_PageNoFunctional.py
+++ T76678_PageNoFunctional.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+import time
 
 class PageNoFunctional(unittest.TestCase):
     def setUp(self):
@@ -10,15 +11,20 @@ class PageNoFunctional(unittest.TestCase):
     def test_PageNoFunctional(self):
         driver = self.driver
         driver.get(self.base_url + "/#/page/login")
+        #Enter Regular User Name
         driver.find_element_by_id("exampleInputEmail1").clear()
         driver.find_element_by_id("exampleInputEmail1").send_keys("avanireg@gmail.com")
+        #Enter Associated password
         driver.find_element_by_id("exampleInputPassword1").clear()
         driver.find_element_by_id("exampleInputPassword1").send_keys("1234567")
+        time.sleep(5)
+        #Click Login Button
         driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.find_element_by_xpath("//div[2]/aside[1]/div/nav/ul/li[2]/a/span").click()
-        #driver.find_element_by_link_text(2).click
-        driver.find_element_by_xpath("//div[2]/section/div/div/div/div[2]/div[2]/table/tbody/tr[12]/td/dir-pagination-controls/ul/li[3]/a").click
-        self.assertEqual("Report 2130",driver.find_element_by_xpath("//div[2]/section/div/div/div/div[2]/div[2]/table/tbody/tr[1]/td/div/div/div/div[1]/h4"))
+        #Click on Reports button from main navigation
+        driver.find_element_by_xpath("//li[2]/a/span").click
+        #click on page2
+        driver.find_element_by_link_text("2").click()
+        print("You are on Page 2")
         driver.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
