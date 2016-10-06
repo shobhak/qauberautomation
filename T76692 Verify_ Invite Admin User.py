ca99b7ecd266e4284b762b079a7038124be14c6e
avanipatel11 <avaniptl@gmail.com>
2016-10-06 16:51:45 -0700

    Add files via upload

--- T76692 Verify_ Invite Admin User.py
+++ T76692 Verify_ Invite Admin User.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 
 class InviteAdminUser(unittest.TestCase):
     def setUp(self):
+        #self.driver = webdriver.Firefox()
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "http://www.testwave.qabidder.net/"
@@ -40,40 +41,46 @@ class InviteAdminUser(unittest.TestCase):
         #print(add)
         # time.sleep(2)
         driver.find_element_by_xpath("//div[3]/input").send_keys("123 cbbnbn")
-        ####### enter city
-        #city = f.city
-        #driver.find_element_by_xpath("//div[5]/input").send_keys(city)
-        #print(city)
-        # time.sleep(2)
-        driver.find_element_by_xpath("//div[5]/input").send_keys("santa clara")
+        ####### enter country
+        driver.find_element_by_xpath("//input[@name='country']").send_keys("USA")
+        ####### enter state
+        #state = f.state()
+        #driver.find_element_by_xpath("//input[contains(@name,'state')]").send_keys(state)
+        #print(state)
+         #driver.find_element_by_xpath("//input[contains(@name,'state')]").send_keys("CA")
         ####### select state from drop down
-        driver.find_element_by_xpath("//a/div/b").click()
-        driver.find_element_by_xpath("//li[@class='active-result'][text()='Kansas - KS']").click()
+        driver.find_element_by_xpath("//select[contains(@ng-show,'ctrl.showStatesDropDown')]").click()
+        driver.find_element_by_xpath("//option[contains(.,'Kansas')]").click()
+        driver.find_element_by_xpath("//option[contains(.,'Kansas')]").click()
+        #driver.find_element_by_xpath("//a/div/b").click()
+        #driver.find_element_by_xpath("//li[@class='active-result'][text()='Kansas - KS']").click()
+        ####### enter city
+        # city = f.city
+        # driver.find_element_by_xpath("//input[contains(@name,'city')]").send_keys(city)
+        # print(city)
+        # time.sleep(2)
+        driver.find_element_by_xpath("//input[contains(@name,'city')]").send_keys("santa clara")
         ####### click NextStep
         driver.find_element_by_xpath("//div[2]/section/div/div/div[2]/button[2]").click()
         print("regular user page")
         time.sleep(2)
-        # click next step
-        #driver.find_element_by_xpath("//button[contains(.,'Nextstep')]").click()
+        ####### click next step
         driver.find_element_by_xpath("//button[2]").click()
         time.sleep(2)
-        driver.find_element_by_xpath("//div[3]/form/div/div/div").click()
         print("Onadmin user page")
-        # add email id for admin user
-        driver.find_element_by_xpath("(//input[@type='text'])[11]").clear()
+        driver.save_screenshot("C:\Users\UNI\Desktop\QAUber")
+        ####### add email id for admin user
         print("enter email id")
-        driver.find_element_by_xpath("(//input[@type='text'])[11]").send_keys("avani.canvas@gmail.com")
-        driver.find_element_by_xpath("(//input[@type='text'])[11]").click()
-        time.sleep(15)
+        driver.find_element_by_xpath("(//input[@type='text'])[12]").send_keys("avani.canvas@gmail.com")
+        driver.find_element_by_xpath("(//input[@type='text'])[12]").click()
         driver.find_element_by_xpath("//button[2]").click()
+        #driver.find_element_by_xpath("//button[@class='btn btn-primary']").is_enabled()
+        ####### click on Invite button
+        #driver.find_element_by_xpath("html/body/div[2]/section/div/div/div[1]/div[3]/form/div/div/span/button").click()
         time.sleep(15)
         print("Invitation sent to admin user")
-
-        #alert = driver.switch_to_alert()
-        #driver.switch_to_window("")
         driver.find_element_by_xpath("//button[contains(.,'Done')]").click()
-        # driver.switch_to_default_content()
-
+        print("done")
         driver.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
