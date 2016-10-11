47a27c0694b3f5bfa082fbce58a3865ed343d2ea
madhutiwari <Madhu.tiwari@gmail.com>
2016-10-11 16:55:37 -0700

    Add files via upload

--- login_wave.java
+++ login_wave.java
@@ -45,9 +45,9 @@ public class login_wave {
         driver.findElement(By.xpath("//span[contains(.,'Add Report')]")).click();
         Thread.sleep(1000); 
         // select radio button for organization
-        driver.findElement(By.xpath("html/body/div[2]/section/div/div/div/form/div/div[2]/fieldset/div[1]/div/div/label")).click();
+        driver.findElement(By.xpath("//label/span")).isSelected();
        // driver.findElement(By.("ng-binding")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         System.out.println("printing");
                 //element.click();
         
@@ -60,16 +60,73 @@ public class login_wave {
         Thread.sleep(1000);
         driver.findElement(By.xpath("//input[contains(@name,'first-name')]")).sendKeys("priya");
         Thread.sleep(1000);
+     // Click on last name
         driver.findElement(By.xpath("//input[contains(@name,'last-name')]")).click(); 
         Thread.sleep(1000);
         driver.findElement(By.xpath("//input[contains(@name,'last-name')]")).sendKeys("dutt"); 
         Thread.sleep(1000);
          driver.findElement(By.xpath("//input[contains(@name,'last-name')]")).click(); 
         Thread.sleep(1000);
+     // select suspect
         driver.findElement(By.xpath("//option[contains(.,'Suspect')]")).click();  
         Thread.sleep(1000);
+     // Click on Next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[3]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
+     // select Checkbox on special sign page
+        driver.findElement(By.xpath("//span/label/span")).isSelected();
+        Thread.sleep(1000);
+     // Click on next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[4]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
         
-         
+        // Click on contacts page
+        driver.findElement(By.xpath("//input[contains(@name,'primary-language')]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[contains(@name,'primary-language')]")).sendKeys("Hindi");;
+        Thread.sleep(1000);
+        // click on address
+        driver.findElement(By.xpath("//input[contains(@name,'street-address')]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[contains(@name,'street-address')]")).sendKeys("213,street");;
+        Thread.sleep(1000);
+        //click on next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[5]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
+        //click on stop location
+        driver.findElement(By.xpath("//input[@name='locationOfStop']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@name='locationOfStop']")).sendKeys("sanata");
+        Thread.sleep(1000);
+        // click on next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[6]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
+        // click on vehicle page
+        driver.findElement(By.xpath("//div[7]/fieldset/div/div/div/label")).isSelected();
+        Thread.sleep(2000);
+        //click on next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[7]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
+        //click on photo page next button
+        driver.findElement(By.xpath("//div/div/div/form/div/div[8]/ul/li[2]/a/span")).click();
+        Thread.sleep(1000);
+        //click on publish report button
+        driver.findElement(By.xpath("//div[9]/ul/li[2]/a")).click();
+        Thread.sleep(2000);
+        //click on users
+        driver.findElement(By.xpath("//div[2]/aside[1]/div/nav/ul/li[4]/a/span")).click();
+        Thread.sleep(2000);
+        //click on invite users button
+        driver.findElement(By.xpath("//div/button")).click();
+        Thread.sleep(2000);
+        //click on Email
+        driver.findElement(By.xpath("//input[@name='email']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("pia@gmail.com");
+        Thread.sleep(2000);
+        // click on send invite button
+        driver.findElement(By.xpath("//button[@type='button']")).click();
+        Thread.sleep(3000);
         
         //Signinbutton.click();
         // close the browser
