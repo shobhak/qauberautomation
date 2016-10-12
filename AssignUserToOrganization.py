482d6994ab7ff5d2ba805696970a8dc4e8ebc9ef
avanipatel11 <avaniptl@gmail.com>
2016-10-11 23:10:40 -0700

    Add files via upload

--- AssignUserToOrganization.py
+++ AssignUserToOrganization.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 import unittest
 from faker import Faker
+from datetime import datetime
 
 class AssignUserToOrgnization(unittest.TestCase):
     def setUp(self):
@@ -32,12 +33,10 @@ class AssignUserToOrgnization(unittest.TestCase):
         ####### enter zipcode
         post =f.postalcode()
         driver.find_element_by_xpath("//div[2]/input").send_keys(post)
-        print(post)
         time.sleep(2)
         ####### enter address
         add =f.street_address()
         driver.find_element_by_xpath("//div[2]/div[1]/div[3]/input").send_keys(add)
-        print(add)
         time.sleep(2)
         ####### enter country
         driver.find_element_by_xpath("//input[@name='country']").send_keys("USA")
@@ -46,20 +45,16 @@ class AssignUserToOrgnization(unittest.TestCase):
         ####### select state from drop down
         driver.find_element_by_xpath("//select[contains(@ng-show,'ctrl.showStatesDropDown')]").click()
         driver.find_element_by_xpath("//select[@ng-show='$ctrl.showStatesDropDown']").send_keys(state)
-        print(state)
         driver.find_element_by_xpath("//select[@ng-show='$ctrl.showStatesDropDown']").click()
         ####### enter city
         city = f.city()
         driver.find_element_by_xpath("//input[contains(@name,'city')]").send_keys(city)
-        print(city)
         ####### click NextStep
         driver.find_element_by_xpath("//div[2]/section/div/div/div[2]/button[2]").click()
-        print("regular user page")
         time.sleep(2)
         ####### click NextStep
         driver.find_element_by_xpath("//button[contains(@ng-click,'ctrl.nextStep()')]").click()
         time.sleep(2)
-        print("Onadmin user page")
         ####### add email id for admin user
         #email = f.email()
         #print(email)
@@ -70,26 +65,28 @@ class AssignUserToOrgnization(unittest.TestCase):
         time.sleep(5)
         ####### click on Invite button
         driver.find_element_by_xpath("//div[2]/section/div/div/div[1]/div[3]/form/div/div/span/button").click()
-        time.sleep(3)
-        print("Invitation sent to admin user")
+        time.sleep(2)
         driver.find_element_by_xpath("//button[contains(.,'Done')]").click()
         time.sleep(5)
-        print("done")
+        newtime = datetime.now().strftime("%Y%m%d%H%M%S")
+        driver.save_screenshot(
+            'C:\Users\UNI\Desktop\QAUber\Automation Assignment\SCREENSHOTS\InviteAdminUser' + newtime + '.png')
         driver.find_element_by_xpath("//button[contains(@ng-click,'ctrl.nextStep()')]").click()
         time.sleep(2)
         driver.find_element_by_xpath("//span[contains(.,'Users')]").click()
         time.sleep(2)
         driver.find_element_by_xpath("//tr[5]/td[1]/em").click()
         time.sleep(2)
-        Actual_OrgName = driver.find_element_by_xpath("//tr[89]/td[3]/div").text
-        self.assertEqual(cname,Actual_OrgName)
-        UserRight = driver.find_element_by_xpath("//tr[89]/td[1]/div").text
-
-        message = "Expected result to be {expected} and actual {actual} : are same".format(
+        Actual_OrgName = driver.find_element_by_xpath("//tr[98]/td[3]/div").text
+        print (Actual_OrgName,"On Users Page")
+        message = "Expected result to be {expected} and actual result is {actual} ".format(
             expected=cname,
             actual=Actual_OrgName
         )
-        self.assertEqual("Admin", UserRight,message)
+        self.assertEqual(cname,Actual_OrgName,message)
+        UserRight = driver.find_element_by_xpath("//tr[98]/td[1]/div").text
+        self.assertEqual("Admin", UserRight)
+        print (Actual_OrgName,"user right is" , UserRight)
         driver.find_element_by_xpath("//button[text()='Update']").click()
         time.sleep(5)
         driver.find_element_by_xpath("//a[contains(.,'Logout')]").click()
