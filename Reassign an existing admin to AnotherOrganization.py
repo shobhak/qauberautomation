b9294d459fa9d41c398fcab978803b77a0e761b2
avanipatel11 <avaniptl@gmail.com>
2016-10-11 23:42:32 -0700

    Add files via upload

--- Reassign an existing admin to AnotherOrganization.py
+++ Reassign an existing admin to AnotherOrganization.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 import unittest
 from faker import Faker
+from datetime import datetime
 
 class AssignUserToOrgnization(unittest.TestCase):
     def setUp(self):
@@ -9,7 +10,7 @@ class AssignUserToOrgnization(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.base_url = "http://www.testwave.qabidder.net/"
 
-    def test_AssignUser(self):
+    def test_ReAssignUser(self):
         f = Faker()
         driver = self.driver
         driver.get(self.base_url + "/#/page/login")
@@ -20,77 +21,28 @@ class AssignUserToOrgnization(unittest.TestCase):
         time.sleep(2)
         driver.find_element_by_xpath("//button[@type='submit']").click()
         time.sleep(2)
-        driver.find_element_by_xpath("//li[5]/a/span").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("html/body/div[2]/section/div/div/button[1]").click()
-        time.sleep(2)
-        ####### write organization name
-        cname = f.company()
-        driver.find_element_by_xpath("//input[contains(@orgname,'orgname')]").send_keys(cname)
-        print(cname)
-        time.sleep(2)
-        ####### enter zipcode
-        post =f.postalcode()
-        driver.find_element_by_xpath("//div[2]/input").send_keys(post)
-        print(post)
-        time.sleep(2)
-        ####### enter address
-        add =f.street_address()
-        driver.find_element_by_xpath("//div[2]/div[1]/div[3]/input").send_keys(add)
-        print(add)
-        time.sleep(2)
-        ####### enter country
-        driver.find_element_by_xpath("//input[@name='country']").send_keys("USA")
-        ####### enter state
-        state = f.state()
-        ####### select state from drop down
-        driver.find_element_by_xpath("//select[contains(@ng-show,'ctrl.showStatesDropDown')]").click()
-        driver.find_element_by_xpath("//select[@ng-show='$ctrl.showStatesDropDown']").send_keys(state)
-        print(state)
-        driver.find_element_by_xpath("//select[@ng-show='$ctrl.showStatesDropDown']").click()
-        ####### enter city
-        city = f.city()
-        driver.find_element_by_xpath("//input[contains(@name,'city')]").send_keys(city)
-        print(city)
-        ####### click NextStep
-        driver.find_element_by_xpath("//div[2]/section/div/div/div[2]/button[2]").click()
-        print("regular user page")
-        time.sleep(2)
-        ####### click NextStep
-        driver.find_element_by_xpath("//button[contains(@ng-click,'ctrl.nextStep()')]").click()
-        time.sleep(2)
-        print("Onadmin user page")
-        ####### add email id for admin user
-        #email = f.email()
-        #print(email)
-        #driver.find_element_by_xpath("(//input[@type='text'])[12]").send_keys(email)
-        driver.find_element_by_xpath("(//input[@type='text'])[12]").send_keys('avani112@gmail.com')
-        ####### u'\ue007' TO PRESS ENTER KEY
-        driver.find_element_by_xpath("(//input[@type='text'])[12]").send_keys(u'\ue007')
-        time.sleep(5)
-        ####### click on Invite button
-        driver.find_element_by_xpath("//div[2]/section/div/div/div[1]/div[3]/form/div/div/span/button").click()
-        time.sleep(3)
-        print("Invitation sent to admin user")
-        driver.find_element_by_xpath("//button[contains(.,'Done')]").click()
-        time.sleep(5)
-        print("done")
-        driver.find_element_by_xpath("//button[contains(@ng-click,'ctrl.nextStep()')]").click()
-        time.sleep(2)
-        #Script to assign user to an organization
+        #Script to re assign user to an organization
         driver.find_element_by_xpath("//span[contains(.,'Users')]").click()
         time.sleep(2)
         #select emailid for admin user
         driver.find_element_by_xpath( "//tr[5]/td[1]/em").click()
         time.sleep(2)
+        Actual_OrgName = driver.find_element_by_xpath("//tr[89]/td[3]/div").text
+        print (Actual_OrgName, "On Users Page")
         # delete admin user for organization
-        driver.find_element_by_xpath("//tr[50]/td[2]/a/i").click()
+        driver.find_element_by_xpath("//tr[89]/td[2]/a/i").click()
+        newtime = datetime.now().strftime("%Y%m%d%H%M%S")
+        driver.save_screenshot(
+            'C:\Users\UNI\Desktop\QAUber\Automation Assignment\SCREENSHOTS\DelAdminUser' + newtime + '.png')
         time.sleep(5)
         #select regular user for organization
-        driver.find_element_by_xpath("//tr[50]/td[1]/select[@ng-model='entity.status']/option[text()='Regular User']").click()
+        driver.find_element_by_xpath("//tr[89]/td[1]/select[@ng-model='entity.status']/option[text()='Regular User']").click()
         time.sleep(2)
         #save the changes
-        driver.find_element_by_xpath("//tr[50]/td[2]/a/i").click()
+        driver.find_element_by_xpath("//tr[89]/td[2]/a/i").click()
+        newtime = datetime.now().strftime("%Y%m%d%H%M%S")
+        driver.save_screenshot(
+            'C:\Users\UNI\Desktop\QAUber\Automation Assignment\SCREENSHOTS\AdmintoRegUser' + newtime + '.png')
         time.sleep(2)
         driver.find_element_by_xpath("//button[text()='Update']").click()
         time.sleep(5)
