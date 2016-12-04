9ffd418111e691eedf95ffcbc4b4329d2bb461a4
erikfriedlander <friedlander.erik@gmail.com>
2016-12-03 18:06:02 -0800

    Testing git

--- src/test/java/SampleTestRailAPITestCaseLog4J2.java
+++ src/test/java/SampleTestRailAPITestCaseLog4J2.java
@@ -1,6 +1,8 @@
 import com.github.javafaker.Faker;
 import com.qauber.config.Config;
 import com.qauber.testrail.APIClientExtension;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +14,10 @@ import org.testng.annotations.Test;
      * Created by sandhu on 11/7/16.
      *
      */
-    public class SampleTestRailAPITestCase {
+
+public class SampleTestRailAPITestCaseLog4J2 {
+
+    private static final Logger logger = LogManager.getLogger(SampleTestRailAPITestCaseLog4J2.class);
 
     //declare object to be shared between methods
     APIClientExtension testrailClient;
@@ -21,20 +26,23 @@ import org.testng.annotations.Test;
     @BeforeClass
     public void setUp() {
 
+        logger.info("setUp");
         testrailClient = Config.testRailLogin();
         runid = Config.getRunID(); //Test Run ID (TestRail - Test Runs & Results - Automation)
-
+        logger.info("setUp done");
     }
 
     @Test
     public void TestError() throws InterruptedException {
+        int caseid = 69246; //Case ID from TestRail - Test Suites & Cases
 
+        logger.info("starting Test \"Invalid login with email field filled and no password and clicking login\"");
         //Create ChromeDriver to drive the browser
         ChromeDriver driver = new ChromeDriver();
 
         //initialization for test
         JSONObject resultsObject;
-        int caseid = 69246; //Case ID from TestRail - Test Suites & Cases
+
 
         // maximize browser
         driver.manage().window().maximize();
@@ -46,6 +54,7 @@ import org.testng.annotations.Test;
 
 
         Faker faker = new Faker();
+        logger.info("faker faker faker faker");
 
         //Find enter email and enter it using faker
         driver.findElement(By.xpath(".//*[@id='exampleInputEmail1']")).sendKeys(faker.internet().emailAddress());
@@ -65,22 +74,26 @@ import org.testng.annotations.Test;
         //  uncomment following line to check for Assert failure...
         actual = "THIS IS NOT THE STRING YOU'RE LOOKING FOR";
 
+        logger.info("ok, let's assert - test if login attempt with empty email field fails and displays validation");
+
         try {
             Assert.assertEquals(actual, expect);
         } catch (AssertionError e) { //on fail, run this
             try {
+                logger.info("Assertion Failed: ", e);
                 testrailClient.addResult(runid, caseid, 5, e.getLocalizedMessage(), "Tester's Computer");
                 throw e; //Throwing AssertionError
             } catch (IllegalStateException g) {
-                g.printStackTrace();
+                logger.error("IllegalStateException ", g);
             }
         } catch (Exception e) {
-            System.out.println("General Exception in assertEquals");
+            logger.error("General Exception in assertEquals", e);
             e.printStackTrace();
 
         }
 
         //If Assert passes, send this to TestRail
+        logger.info("assert successful, let's upload to testrail");
         testrailClient.addResult(runid, caseid, 1, "Actual value = expected value", "Tester's Computer");
 
         Thread.sleep(3000);
