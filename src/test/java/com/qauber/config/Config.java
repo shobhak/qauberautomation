f20e37c771a1cbcdbd021c07f3b4c7eb6ae5c1b0
erikfriedlander <friedlander.erik@gmail.com>
2016-12-13 06:15:56 -0800

    Testing git

--- src/test/java/com/qauber/config/Config.java
+++ src/test/java/com/qauber/config/Config.java
@@ -1,6 +1,7 @@
 package com.qauber.config;
 
 import com.qauber.testrail.APIClientExtension;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * Created by erikfriedlander on 11/29/16.
@@ -8,10 +9,14 @@ import com.qauber.testrail.APIClientExtension;
 public class Config {
 
     private final static int RUNID = 528;
+    private final static String BASEURL = "http://testwave.qabidder.net";
+    private final static String LOG = "/log/test.txt";
 
     public static int getRunID() {
         return RUNID;
     }
+
+    public static String getBaseURL() { return BASEURL; }
 
     public static APIClientExtension testRailLogin() { //passes back a logged in client
         APIClientExtension client = new APIClientExtension("https://bidqa.testrail.net/");
@@ -19,6 +24,25 @@ public class Config {
         client.setPassword("qauber2016!");
 
         return client;
+    }
+
+    public static String setLogFile(String CustomLog) {
+        System.setProperty("logFilename", CustomLog);
+
+        org.apache.logging.log4j.core.LoggerContext ctx =
+                (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
+        ctx.reconfigure();
+
+        return CustomLog;
+    }
+
+    public static String setLogFile() {
+        setLogFile(LOG);
+        return LOG;
+    }
+
+    public static String getLogFile() {
+        return LOG;
     }
 
 }
