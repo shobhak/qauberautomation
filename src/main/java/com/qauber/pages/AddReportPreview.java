7c2c6d3be5e163584d4f488d96967673d29ecf62
Alya <alyavs17@gmail.com>
2016-12-19 10:53:17 -0800

    AddReportPreview page created

---
+++ src/main/java/com/qauber/pages/AddReportPreview.java
@@ -0,0 +1,27 @@
+package com.qauber.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Alya on 12/19/2016.
+ */
+public class AddReportPreview {
+
+    private WebDriver driver;
+
+    public AddReportPreview(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public WebElement publishReportButton() {
+        return driver.findElement(By.xpath("//a[@ng-click= \"wizard.commitReport()\"]"));
+    }
+
+    public WebElement previousButton() {
+        return driver.findElement(By.name("html/body/div[2]/section/div/div/div/form/div/div[9]/ul/li[1]/a/span"));
+    }
+}
+
