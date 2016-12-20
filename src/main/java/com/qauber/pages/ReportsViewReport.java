f5c8d77336bf3fee6f85bdb1594b5c52c30bed8e
Alya <alyavs17@gmail.com>
2016-12-20 14:14:57 -0800

    Created ReportsViewReport

---
+++ src/main/java/com/qauber/pages/ReportsViewReport.java
@@ -0,0 +1,29 @@
+package com.qauber.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Alya on 12/20/2016.
+ */
+public class ReportsViewReport {
+    private WebDriver driver;
+
+    public ReportsViewReport(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public WebElement viewReportButton() {
+        return driver.findElement(By.xpath("html/body/div[2]/section/div/div/div/div[2]/div[2]/table/tbody/tr[1]/td/div/div/div[2]/div[4]/a"));
+    }
+
+    public static WebElement downloadAsPDFButton(WebDriver driver) {
+        return driver.findElement(By.xpath("//button[text()='Download as PDF']"));
+    }
+
+    public static WebElement useAsTemplateButton(WebDriver driver) {
+        return driver.findElement(By.xpath("//button[text()='Use as template']"));
+    }
+}
