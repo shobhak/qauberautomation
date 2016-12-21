af629cced8f7f13c23c2f25b528eb59c37bc32db
Alya <alyavs17@gmail.com>
2016-12-21 11:54:34 -0800

    ReportsExportReportListAsPDFTestCase created

---
+++ src/test/java/com/qauber/pagestest/ReportsExportReportListAsPDFTestCase.java
@@ -0,0 +1,35 @@
+package com.qauber.pagestest;
+
+import com.qauber.config.Config;
+import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.User;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alya on 12/20/2016.
+ */
+public class ReportsExportReportListAsPDFTestCase extends PageObjectModelResources{
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        setUpWithUser(User.UserType.SAU, driver);
+    }
+
+    @Test
+    public void reportsExportReportListAsPDF() throws InterruptedException {
+        driver.get(Config.getBaseURL());
+        Thread.sleep(10000);
+        getLogin().loginToWave(getTestCaseUser().getUsername(), getTestCaseUser().getPassword());
+        Thread.sleep(5000);
+        getReports().exportReportsListButton().click();
+    }
+    /*@AfterClass
+    public void breakDown(){
+        breakDownHelper(driver);
+    }*/
+}
