02c461581951277cb28654408a0a219168aaa35d
Alya <alyavs17@gmail.com>
2016-12-22 14:32:15 -0800

    Updated

--- src/main/java/com/qauber/pages/Reports.java
+++ src/main/java/com/qauber/pages/Reports.java
@@ -133,6 +133,18 @@ public class Reports {
         return driver.findElement(By.cssSelector(".btn.btn-labeled.btn-primary"));
     }
 
+    public WebElement searchReportResultID(int rowNumber){
+        return driver.findElement(By.xpath("//tr["+rowNumber+"]//div[@class='row'][1]/div[@class='col-lg-12']/h4[@class='ng-binding']"));
+    }
+
+    public boolean verifyReportPublished(String reportNumber) throws InterruptedException {
+        //from Reports page...
+        //search for reports which contain text (reportNumber)
+        containsTextField().sendKeys(reportNumber);
+        Thread.sleep(3000);
+        System.out.println(searchReportResultID(1).getText());
+        return searchReportResultID(1).getText().contains(reportNumber);
+    }
 
 
 
