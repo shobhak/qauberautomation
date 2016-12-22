ae8aa2293581dfc332a23bab0864ed7b90183125
Alya <alyavs17@gmail.com>
2016-12-22 10:02:17 -0800

    Created HeaderSiteMenuCollapsibleTestCase

---
+++ src/test/java/com/qauber/pagestest/HeaderSiteMenuCollapsibleTestCase.java
@@ -0,0 +1,47 @@
+package com.qauber.pagestest;
+
+import com.qauber.config.Config;
+import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.User;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alya on 12/21/2016.
+ */
+public class HeaderSiteMenuCollapsibleTestCase extends PageObjectModelResources {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        setUpWithUser(User.UserType.SAU, driver);
+    }
+
+    @Test
+    public void siteMenuCollapsible() throws InterruptedException {
+        driver.get(Config.getBaseURL());
+        Thread.sleep(10000);
+        getLogin().loginToWave(getTestCaseUser().getUsername(), getTestCaseUser().getPassword());
+        Thread.sleep(5000);
+
+        //collapse
+        getHeader().hamburgerMenu().click();
+        Thread.sleep(1000);
+        Assert.assertFalse(getNavBar().mainNavigationText().isDisplayed());
+
+        //uncollapse
+        getHeader().hamburgerMenu().click();
+        Thread.sleep(1000);
+        Assert.assertTrue(getNavBar().mainNavigationText().isDisplayed());
+    }
+
+    @AfterClass
+    public void breakDown(){
+        breakDownHelper(driver);
+    }
+}
