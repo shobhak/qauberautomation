1a70dcf158ec98684f8335d6da6dec070048460b
Alya <alyavs17@gmail.com>
2016-12-27 13:45:00 -0800

    Updated

--- src/test/java/com/qauber/pagestest/AddReportPreviewPublishTestCase.java
+++ src/test/java/com/qauber/pagestest/AddReportPreviewPublishTestCase.java
@@ -1,20 +1,16 @@
 package com.qauber.pagestest;
 
-import com.github.javafaker.Faker;
 import com.qauber.config.Config;
-import com.qauber.pages.*;
 import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.ReportValueObject;
 import com.qauber.pagesresource.User;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Alya on 12/19/2016.
@@ -24,30 +20,9 @@ import java.util.concurrent.TimeUnit;
 
 public class AddReportPreviewPublishTestCase extends PageObjectModelResources {
 
-    /*private WebDriver driver;
-
-    private LoginPage login;
-    private AddReportsOrganization addReportsOrganization;
-    private NavBar navBar;
-    private AddReportNavigation addReportNavigation;
-    private AddReportPreview addReportPreview;
-    private AddReportSubjectInformationPage addReportSubjectInformationPage;
-    private AddReportEnvironment addReportEnvironment;
-    private Reports reports;
-
-    @BeforeClass
-    public void setUp() {
-        driver = new ChromeDriver();
-        //implicit wait
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //pull URL from config
-        driver.get(Config.getBaseURL());
-        //maximize window for our viewing pleasure
-        driver.manage().window().maximize();
-    }*/
-
     WebDriver driver;
     String reportIdValue;
+    String firstLastName;
 
     @BeforeClass
     public void setUp() {
@@ -60,43 +35,27 @@ public class AddReportPreviewPublishTestCase extends PageObjectModelResources {
         driver.get(Config.getBaseURL());
         Thread.sleep(10000);
 
-        Faker faker = new Faker();
+        ReportValueObject reportVO = new ReportValueObject();
+        firstLastName = reportVO.getFirstName()+" "+reportVO.getLastName();
 
         getLogin().loginToWave(getTestCaseUser().getUsername(), getTestCaseUser().getPassword());
         Thread.sleep(5000);
 
-        //Create all Page Objects
-        /*login = new LoginPage(driver);
-        navBar = new NavBar(driver);
-        addReportsOrganization = new AddReportsOrganization(driver);
-        addReportNavigation = new AddReportNavigation(driver);
-        addReportPreview = new AddReportPreview(driver);
-        addReportSubjectInformationPage = new AddReportSubjectInformationPage(driver);
-        addReportEnvironment = new AddReportEnvironment(driver);
-        reports = new Reports(driver);
-
-        //Log in to application
-        Thread.sleep(5000);
-        login.loginToWave("erikfqauber@gmail.com", "testwave");
-        Thread.sleep(3000);*/
-
-        //Go to 'Add Report'
         getNavBar().addReportButton().click();
-
-        //Select first organization
         Thread.sleep(1000);
+
         getAddReportOrganization().clickOrganization1();
         Thread.sleep(1000);
 
         getAddReportNavigation().subjectInformationTab().click();
         Thread.sleep(1000);
-        getAddReportSubjectInformationPage().firstName().sendKeys(faker.name().firstName());
-        getAddReportSubjectInformationPage().lastName().sendKeys(faker.name().lastName());
+        getAddReportSubjectInformationPage().firstName().sendKeys(reportVO.getFirstName());
+        getAddReportSubjectInformationPage().lastName().sendKeys(reportVO.getLastName());
         Thread.sleep(1000);
 
         getAddReportNavigation().environmentTab().click();
         Thread.sleep(1000);
-        getAddReportEnvironment().stopLocationField().sendKeys(faker.address().city());
+        getAddReportEnvironment().stopLocationField().sendKeys(reportVO.getStopLocation());
         Thread.sleep(1000);
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -105,18 +64,21 @@ public class AddReportPreviewPublishTestCase extends PageObjectModelResources {
         getAddReportNavigation().previewTab().click();
         Thread.sleep(1000);
 
+        Assert.assertEquals(getAddReportPreview().suspectName().getText(),firstLastName);
+        Assert.assertEquals(getAddReportPreview().stopLocation().getText(), reportVO.getStopLocation());
+
         reportIdValue = getAddReportPreview().reportIdElement().getText();
 
         //caseID
         getAddReportPreview().editPencilIcon().click();
         Thread.sleep(1000);
-        getAddReportPreview().caseIDField().sendKeys(faker.number().digit());
+        getAddReportPreview().caseIDField().sendKeys(reportVO.getCaseID());
         Thread.sleep(1000);
         getAddReportPreview().cancelCaseIDButton().click();
         Thread.sleep(1000);
         getAddReportPreview().editPencilIcon().click();
         Thread.sleep(1000);
-        getAddReportPreview().caseIDField().sendKeys(faker.number().digit());
+        getAddReportPreview().caseIDField().sendKeys(reportVO.getCaseID());
         Thread.sleep(1000);
         getAddReportPreview().saveCaseIDButton().click();
         Thread.sleep(1000);
