b50adfb3b203c00b846c989ef0060a43c3211392
Alya <alyavs17@gmail.com>
2016-12-29 00:28:42 -0800

    AddReport created

---
+++ src/test/java/com/qauber/sanity/AddReport.java
@@ -0,0 +1,92 @@
+package com.qauber.sanity;
+
+import com.qauber.config.Config;
+import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.ReportValueObject;
+import com.qauber.pagesresource.User;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alya on 12/19/2016.
+ */
+
+//creating report using required fields
+
+public class AddReport extends PageObjectModelResources {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        setUpWithUser(User.UserType.SAU, driver);
+    }
+
+    @Test
+    public void addReportPreviewPublish () throws InterruptedException {
+        driver.get(Config.getBaseURL());
+        Thread.sleep(10000);
+
+        ReportValueObject reportVO = new ReportValueObject();
+        String firstLastName = reportVO.getFirstName()+" "+reportVO.getLastName();
+
+        getLogin().loginToWave(getTestCaseUser().getUsername(), getTestCaseUser().getPassword());
+        Thread.sleep(5000);
+
+        getNavBar().addReportButton().click();
+        Thread.sleep(1000);
+
+        getAddReportOrganization().clickOrganization1();
+        Thread.sleep(1000);
+
+        getAddReportNavigation().subjectInformationTab().click();
+        Thread.sleep(1000);
+        getAddReportSubjectInformationPage().firstName().sendKeys(reportVO.getFirstName());
+        getAddReportSubjectInformationPage().lastName().sendKeys(reportVO.getLastName());
+        Thread.sleep(1000);
+
+        getAddReportNavigation().environmentTab().click();
+        Thread.sleep(1000);
+        getAddReportEnvironment().stopLocationField().sendKeys(reportVO.getStopLocation());
+        Thread.sleep(1000);
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollBy(0,-1000)", "");
+
+        getAddReportNavigation().previewTab().click();
+        Thread.sleep(1000);
+
+        //verify info entered matches info on preview
+        Assert.assertEquals(getAddReportPreview().suspectName().getText(),firstLastName);
+        Assert.assertEquals(getAddReportPreview().stopLocation().getText(), reportVO.getStopLocation());
+
+        //verify report published
+        String reportIdValue = getAddReportPreview().reportIdElement().getText();
+
+        getAddReportPreview().publishReportButton().click();
+        Thread.sleep(1000);
+
+        Assert.assertTrue(verifyReportPublished(reportIdValue));
+    }
+
+    public boolean verifyReportPublished(String reportNumber) throws InterruptedException {
+        //search for reports which contain text (reportNumber)
+        getReports().containsTextField().sendKeys(reportNumber);
+        Thread.sleep(3000);
+        System.out.println(getReports().searchReportResultID(1).getText());
+        return getReports().searchReportResultID(1).getText().contains(reportNumber);
+    }
+
+    @AfterClass
+    public void breakDown () throws InterruptedException {
+        Thread.sleep(15000);
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+}
