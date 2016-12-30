6c020fd1d6439f4306f49341971189ecdabe3695
Jing Xu <jxugithub>
2016-12-29 17:09:06 -0800

    restore the original slider method, and create a new sliderByPercent method

--- src/main/java/com/qauber/pages/SubscriptionSettings.java
+++ src/main/java/com/qauber/pages/SubscriptionSettings.java
@@ -37,28 +37,34 @@ public class SubscriptionSettings {
         WebElement slider = driver.findElement(By.cssSelector(".slider-handle.min-slider-handle.round"));
         WebElement element = driver.findElement(By.xpath("//div[@class='slider-handle min-slider-handle round']"));
         Actions move = new Actions(driver);
+        Action action;
+        int i = 0;
+        if (Integer.parseInt(element.getAttribute("aria-valuenow"))<=x)
+        {
+            while (Integer.parseInt(element.getAttribute("aria-valuenow")) != x) {
+                action = move.dragAndDropBy(slider, i, 0).build();
+                action.perform();
+                i++;
+            }
+        }
+        else
+        {
+            while (Integer.parseInt(element.getAttribute("aria-valuenow")) != x) {
+                action = move.dragAndDropBy(slider, i, 0).build();
+                action.perform();
+                i--;
+            }
+        }
+    }
+
+    public void sliderByPercent(int x) throws InterruptedException {
+        WebElement slider = driver.findElement(By.cssSelector(".slider-handle.min-slider-handle.round"));
+        WebElement element = driver.findElement(By.xpath("//div[@class='slider-handle min-slider-handle round']"));
+        Actions move = new Actions(driver);
         Action action = (Action) move.dragAndDropBy(slider, -300, 0).build();
         action.perform();
         Thread.sleep(10000);
         Action action2 = (Action) move.dragAndDropBy(slider, x, 0).build();
         action2.perform();
-//        Action action;
-//        int i = 0;
-//        if (Integer.parseInt(element.getAttribute("aria-valuenow"))<=x)
-//        {
-//            while (Integer.parseInt(element.getAttribute("aria-valuenow")) != x) {
-//                action = move.dragAndDropBy(slider, i, 0).build();
-//                action.perform();
-//                i++;
-//            }
-//        }
-//        else
-//        {
-//            while (Integer.parseInt(element.getAttribute("aria-valuenow")) != x) {
-//                action = move.dragAndDropBy(slider, i, 0).build();
-//                action.perform();
-//                i--;
-//            }
-//        }
     }
 }
