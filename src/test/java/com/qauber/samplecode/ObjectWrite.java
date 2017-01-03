c8f373561d800b1f0064a4c75b6eea100c03bccc
erikfriedlander <friedlander.erik@gmail.com>
2017-01-03 14:20:23 -0800

    Create ObjectWrite.java

---
+++ src/test/java/com/qauber/samplecode/ObjectWrite.java
@@ -0,0 +1,18 @@
+package com.qauber.samplecode;
+
+import com.qauber.pagesresource.FileManager;
+import com.qauber.pagesresource.User;
+
+/**
+ * Created by erikfriedlander on 12/29/16.
+ */
+public class ObjectWrite {
+    public static void main(String args[]) {
+
+
+        User user = new User();
+        user.setUsername("Denys@bestemail.com");
+        user.setPassword("bestPassword");
+        FileManager.writeConfigObject(user, "newUser.txt", "These are very important comments");
+    }
+}
