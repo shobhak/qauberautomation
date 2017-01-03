f116f51113c16b09d951ff9bc659927432d67cf8
Alya <alyavs17@gmail.com>
2017-01-02 22:18:50 -0800

    Updated

--- src/test/java/com/qauber/sanity/SiteMenuCollapsible.java
+++ src/test/java/com/qauber/sanity/SiteMenuCollapsible.java
@@ -1,10 +1,7 @@
 package com.qauber.sanity;
 
-import com.qauber.config.Config;
 import com.qauber.pagesresource.PageObjectModelResources;
 import com.qauber.pagesresource.User;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -14,17 +11,17 @@ import org.testng.annotations.Test;
  * Created by Alya on 12/21/2016.
  */
 public class SiteMenuCollapsible extends PageObjectModelResources {
-    WebDriver driver;
 
     @BeforeClass
     public void setUp() {
-        driver = new ChromeDriver();
-        setUpWithUser(User.UserType.SAU, driver);
+        setUpWithConfigFile();
+        setUpUser(User.UserType.SAU);
+        setUpScript();
     }
 
     @Test
     public void siteMenuCollapsible() throws InterruptedException {
-        driver.get(Config.getBaseURL());
+        testDriver().get(testConfig().getBaseURL());
         Thread.sleep(10000);
         getLogin().loginToWave(testUser().getUsername(), testUser().getPassword());
         Thread.sleep(5000);
@@ -41,7 +38,7 @@ public class SiteMenuCollapsible extends PageObjectModelResources {
     }
 
     @AfterClass
-    public void breakDown(){
-        breakDownHelper(driver);
+    public void breakDown() {
+        breakDownHelper();
     }
 }
