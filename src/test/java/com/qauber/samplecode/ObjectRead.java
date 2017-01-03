990b115b2939118306bfa7e13962eccfb351d87a
erikfriedlander <friedlander.erik@gmail.com>
2017-01-03 14:19:56 -0800

    well, I uploaded the files with names switched

--- src/test/java/com/qauber/samplecode/ObjectRead.java
+++ src/test/java/com/qauber/samplecode/ObjectRead.java
@@ -3,17 +3,18 @@ package com.qauber.samplecode;
 import com.qauber.pagesresource.FileManager;
 import com.qauber.pagesresource.User;
 
+import java.io.IOException;
+
 /**
- * Created by erikfriedlander on 12/29/16.
+ * Created by erikfriedlander on 1/3/17.
  */
 public class ObjectRead {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
 
+        User user = FileManager.getConfigObject("newUser.txt", User.class);
 
-        User user = new User();
-        user.setUsername("Denys@bestemail.com");
-        user.setPassword("bestPassword");
-        FileManager.writeConfigObject(user, "newUser.txt", "These are very important comments");
-//        driver.quit();
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
     }
+
 }
