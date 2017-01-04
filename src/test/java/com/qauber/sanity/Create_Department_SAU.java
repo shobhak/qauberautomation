1c41b7d00852897943ec201d98141103d234a3b4
San Hose <ramazan@bigmir.net>
2017-01-04 03:41:07 +0300

    Merge branch 'Round13POM' of https://github.com/qauber/qauberautomation into Round13POM
    # Conflicts:
    #	src/test/java/com/qauber/sanity/Create_Department_SAU.java

--- src/test/java/com/qauber/sanity/Create_Department_SAU.java
+++ src/test/java/com/qauber/sanity/Create_Department_SAU.java
@@ -1,10 +1,13 @@
 package com.qauber.sanity;
 
+import com.github.javafaker.Faker;
 import com.qauber.config.Config;
 import com.qauber.pagesresource.PageObjectModelResources;
 import com.qauber.pagesresource.User;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -28,22 +31,31 @@ public class Create_Department_SAU extends PageObjectModelResources {
     @Test
     public void createDepartmentSAU() throws InterruptedException{
         driver.get(Config.getBaseURL());
+        Faker faker = new Faker();
+
+        String expectedResult = faker.lorem().word(); //department name
+        String actualResult;
+
         getLogin().loginToWave(testUser().getUsername(), testUser().getPassword());
         Thread.sleep(sleepTime);
         getNavBar().entitiesButton().click();
         Thread.sleep(sleepTime);
-        getEntities().organizationList().get(1).click();
+        getEntities().organizationList().get(0).click();
         Thread.sleep(sleepTime);
-//        getOrganization().
+        getEntities().AddDepartmentButton().click();
+        Thread.sleep(sleepTime);
+        getEntities().AddDepartmentField().sendKeys(expectedResult);
+        getEntities().CreatDepartmentButton().click();
+        Thread.sleep(sleepTime);
+
+//        actualResult, expectedResult
+
+        int lastIndex = getEntities().organizationList().size()-1;
+        actualResult = getEntities().organizationList().get(lastIndex).getText();
+        Assert.assertEquals(expectedResult, actualResult);
 
 
     }
-
-
-
-
-
-
 
     @AfterClass
     public void breakDown(){
