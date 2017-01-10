952680fc6172346a9531cd071717f20ba9a9ee0b
Max <raizzz013@gmail.com>
2017-01-10 15:49:03 -0800

    Added ReportPrecondition into TCs. Max

--- src/test/java/com/qauber/sanity/SearchReportsByCreatedDate.java
+++ src/test/java/com/qauber/sanity/SearchReportsByCreatedDate.java
@@ -39,13 +39,16 @@ public class SearchReportsByCreatedDate extends PageObjectModelResources {
     public void searchReportsByCreatedDate() throws InterruptedException
     {
         getNavBar().reportsButton().click();
-        Thread.sleep(sleepTime*2);
-
-        date = getPreconditions().getSearchHelper().randomDate();
+        Thread.sleep(sleepTime);
         getReports().publishedOnCheckBox().click();
+        Thread.sleep(sleepTime);
+
+        getPreconditions().getReportPreconditions().ensureReportsAtLeast(10);
+        date = getPreconditions().getSearchHelper().randomDate();
+
         getReports().createdDateFromIcon(date);
         getReports().createdDateToIcon(date);
-        Thread.sleep(sleepTime*2);
+        Thread.sleep(sleepTime);
 
         //TODO: Found the way to assert that.
     }
