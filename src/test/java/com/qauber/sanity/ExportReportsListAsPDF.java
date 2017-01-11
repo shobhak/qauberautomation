d2cc05fc731005bcf831e97d5d0336e32608cbae
Alya <alyavs17@gmail.com>
2017-01-11 00:00:53 -0800

    Integrated with testrail

--- src/test/java/com/qauber/sanity/ExportReportsListAsPDF.java
+++ src/test/java/com/qauber/sanity/ExportReportsListAsPDF.java
@@ -2,6 +2,7 @@ package com.qauber.sanity;
 
 import com.qauber.pagesresource.FileManager;
 import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.TestRail;
 import com.qauber.pagesresource.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,10 +13,6 @@ import java.io.File;
 
 /**
  * Created by Alya on 12/20/2016.
- * TODO: add TestRail integration
- * TODO: precondition? check for condition before download?
- * (note from Erik) If you face saving-file challenges in the future, look at this:
- * http://ardesco.lazerycode.com/index.php/2012/07/how-to-download-files-with-selenium-and-why-you-shouldnt/
  */
 public class ExportReportsListAsPDF extends PageObjectModelResources{
 
@@ -27,6 +24,8 @@ public class ExportReportsListAsPDF extends PageObjectModelResources{
         setUpWithConfigFile();
         setUpUser(User.UserType.SAU);
         sleepTime = testConfig().getSleepTime();
+        testConfig().getTestRail().setCaseID(82779);
+        testConfig().getTestRail().setTester("Alya");
         setUpScript();
     }
 
@@ -37,16 +36,25 @@ public class ExportReportsListAsPDF extends PageObjectModelResources{
         getLogin().loginToWave(testUser().getUsername(), testUser().getPassword());
         Thread.sleep(sleepTime);
         getReports().publishedOnCheckBox().click();
+        getPreconditions().getReportPreconditions().ensureReportsAtLeast(1);
         Thread.sleep(sleepTime/2);
 
-        String fileName = downloadPath + "/search_result.pdf";
+        String fileName = downloadPath + "search_result.pdf";
         deleteFile(fileName);
 
         getReports().exportReportsListButton().click();
         Thread.sleep(sleepTime/2);
 
+        try {
         Assert.assertTrue(isFileExported(fileName), "Failed to export Expected document");
         Thread.sleep(sleepTime);
+        } catch (AssertionError e) {
+            testConfig().getTestRail().addResults(TestRail.TestCaseResult.FAILED, "File wasn't downloaded "+e.getLocalizedMessage());
+            throw e;
+        }
+
+        testConfig().getTestRail().addResults(TestRail.TestCaseResult.PASSED, "Test passed");
+
     }
 
     public boolean isFileExported(String fileName) {
