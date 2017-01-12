ce5a69bc3023758bddf5e0b3be4df65a98ee4409
Alya <alyavs17@gmail.com>
2017-01-11 21:16:28 -0800

    Updated

--- src/main/java/com/qauber/assertutil/AssertUber.java
+++ src/main/java/com/qauber/assertutil/AssertUber.java
@@ -3,16 +3,17 @@ package com.qauber.assertutil;
 import com.qauber.pagesresource.ConfigHelper;
 import com.qauber.pagesresource.ConfigOOP;
 import com.qauber.pagesresource.TestRail;
+import org.testng.Assert;
 
 /**
  * Created by Alya on 1/11/2017.
  */
-public class Assert {
+public class AssertUber {
     private static ConfigOOP config = ConfigHelper.getConfigFile();
 
     public static void assertTrue(boolean conditions, String message){
         try {
-            org.testng.Assert.assertTrue(conditions, message);
+            Assert.assertTrue(conditions, message);
         } catch (AssertionError e) {
             config.getTestRail().addResults(TestRail.TestCaseResult.FAILED, message);
             throw e;
