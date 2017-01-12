bfd9a7e43f4880a0a37d5aa9c9a898fcef5f96c3
deniskogol <deniskogol@gmail.com>
2017-01-12 15:59:52 -0800

    added TESTRAIL results + moved asserts to TEST

--- src/test/java/com/qauber/sanity/UsersAssignPermissions.java
+++ src/test/java/com/qauber/sanity/UsersAssignPermissions.java
@@ -1,7 +1,9 @@
 package com.qauber.sanity;
 
 import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.TestRail;
 import com.qauber.pagesresource.User;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,6 +17,9 @@ import java.awt.*;
 public class UsersAssignPermissions extends PageObjectModelResources {
 
     private int sleepTime;
+    /** NOTE: index in the list is different while logged in as different users*/
+    int userIndex = 2;
+    int entityIndex = 1;
 
     /** NOTE: Preconditions about existing entities and admin assigned should be added*/
 
@@ -28,25 +33,46 @@ public class UsersAssignPermissions extends PageObjectModelResources {
 
     @Test
     public void testAsSAU() throws AWTException, InterruptedException {
+        testConfig().getTestRail().setCaseID(82783);
+        testConfig().getTestRail().setTester("Denys_G");
         setUpUser(User.UserType.SAU); //pass userType and browser. see ~/QAUberTestConfig
         //setUpWithUser creates TestCaseUser, access with testUser()
         setUpScript();
         assignPermissionsUsers();
-        breakDownHelper();
+
+        try {
+            Assert.assertEquals(getUsersPermissionsDialog().rightsSavedTR(entityIndex).getText(),
+                    "Admin");//method setUserAsAdmin
+        } catch (AssertionError e) {
+            testConfig().getTestRail().addResults(TestRail.TestCaseResult.FAILED, "Other permission shown "+e.getLocalizedMessage());
+            throw e;
+        }
+        testConfig().getTestRail().addResults(TestRail.TestCaseResult.PASSED, "Test passed");
+
+        breakDownHelper();//used to close window
     }
     @Test(priority = 1)
     public void testAsAU() throws AWTException, InterruptedException {
+        testConfig().getTestRail().setCaseID(82785);
+        testConfig().getTestRail().setTester("Denys_G");
         setUpUser(User.UserType.AU); //pass userType and browser. see ~/QAUberTestConfig
         //setUpWithUser creates TestCaseUser, access with testUser()
         setUpScript();
         assignPermissionsUsers();
+
+
+        try {
+            Assert.assertEquals(getUsersPermissionsDialog().rightsSavedTR(entityIndex).getText(),
+                    "Admin");//method setUserAsAdmin
+        } catch (AssertionError e) {
+            testConfig().getTestRail().addResults(TestRail.TestCaseResult.FAILED, "Other permission shown "+e.getLocalizedMessage());
+            throw e;
+        }
+
+        testConfig().getTestRail().addResults(TestRail.TestCaseResult.PASSED, "Test passed");
     }
 
     public void assignPermissionsUsers() throws InterruptedException, AWTException {
-
-        /** NOTE: index in the list is different while logged in as different users*/
-        int userIndex = 2;
-        int entityIndex = 1;
 
         testDriver().get(testConfig().getBaseURL());
         Thread.sleep(sleepTime/3);
@@ -61,7 +87,11 @@ public class UsersAssignPermissions extends PageObjectModelResources {
         getUsersPermissionsDialog().setUserAsAdminTR(entityIndex);
         Thread.sleep(sleepTime/8);
         getUsersPermissionsDialog().closeDialogByPressESC();
+        Thread.sleep(sleepTime/4);
+        getUsers().assignPermissionsButtonByIndex(userIndex).click();
         Thread.sleep(sleepTime/8);
+
+
     }
 
     @AfterTest
