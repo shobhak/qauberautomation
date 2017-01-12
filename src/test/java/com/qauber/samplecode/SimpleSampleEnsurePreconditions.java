477a1f062789457fe758be84eb1366af7c7a3d99
erikfriedlander <friedlander.erik@gmail.com>
2017-01-11 17:10:17 -0800

    update comments in 'SimpleSampleEnsurePreconditions'

--- src/test/java/com/qauber/samplecode/SimpleSampleEnsurePreconditions.java
+++ src/test/java/com/qauber/samplecode/SimpleSampleEnsurePreconditions.java
@@ -7,32 +7,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
- * Created by erikfriedlander on 12/24/16.
- * === This test case demonstrates the use of a Config object with a configuration text file and TestRail integration.
- * === It reports to TestRail whether the test passes or fails, with useful results - see try/catch at Assert.
- *
- * TEST RAIL CASE: https://bidqa.testrail.net/index.php?/cases/view/79853
- * TEST RAIL RESULTS: https://bidqa.testrail.net/index.php?/cases/results/79853
- *
- * We test if 'Add Entity' button text on top of Entity page is correct.
- *
- * Setup:
- * 1. reads test config from config.txt
- * 2. gets a 'test user'
- *** 3. sets test case ID & 'tester name' for TestRail integration through config
- * 4. makes local sleepTime
- * 5. finishes setup (creates driver & POM objects)
- *
- * ===========
- * Test:
- * Assert if text matches.
- **** IF FAIL: Catch failed assertion (AssertionError) and report results:
- * testConfig().getTestRail().addResults(TestRail.TestCaseResult.FAILED, "(useful failed test results comment");
- * and throw back AssertionError to TestNG.
- **** IF SUCCEED: Assertion succeeds
- * testConfig().getTestRail().addResults(TestRail.TestCaseResult.PASS, "(some comments here");
- *
- * AddResults will eventually upload the results to TestRail.
+ * Created by erikfriedlander on 1/03/16.
+ * This test case demonstrates the use of preconditions.
  */
 public class SimpleSampleEnsurePreconditions extends PageObjectModelResources {
 
@@ -54,14 +30,18 @@ public class SimpleSampleEnsurePreconditions extends PageObjectModelResources {
     @Test
     public void ensureUsersAtLeastPreconditionTest() throws InterruptedException {
         testDriver().get(testConfig().getBaseURL());
-        Thread.sleep(sleepTime);
+//        Thread.sleep(sleepTime);
 
         //Log in...
         getLogin().loginToWave(testUser().getUsername(), testUser().getPassword());
         Thread.sleep(sleepTime);
 
+        getNavBar().clickEntities();
+
         ///////ENSURE PRECONDITION: AT LEAST n USERS
-        getPreconditions().getUsersPreconditions().ensureUsersAtLeast(30);
+        getPreconditions().getUsersPreconditions().ensureUsersAtLeast(28);
+//        getPreconditions().getSamplePreconditionsAsSeparateUser().ensureUsersAtLeast(22);
+//        getPreconditions().getSamplePreconditionsAsSeparateUser().ensureUsersAtLeast(24);
 
     }
 
