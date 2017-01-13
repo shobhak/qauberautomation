f1dac8d9713d9991a0784114ba3c4a765c2e4c2f
deniskogol <deniskogol@gmail.com>
2017-01-13 15:18:35 -0800

    TestRail integration. Asserts added. Added preconditions method to first @test(SAU) which sets preconditions needed for @test2(AU)

--- src/test/java/com/qauber/sanity/EntitiesAssignPermissions.java
+++ src/test/java/com/qauber/sanity/EntitiesAssignPermissions.java
@@ -1,7 +1,9 @@
 package com.qauber.sanity;
 
 import com.qauber.pagesresource.PageObjectModelResources;
+import com.qauber.pagesresource.TestRail;
 import com.qauber.pagesresource.User;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -10,9 +12,6 @@ import java.awt.*;
 
 /**
  * Created by Denys_G on 12/21/16.
- * TODO: Add Assertions
- * TODO: TestRail Integration
- * TODO: preconditions?
  */
 
 public class EntitiesAssignPermissions extends PageObjectModelResources {
@@ -31,20 +30,27 @@ public class EntitiesAssignPermissions extends PageObjectModelResources {
 
     @Test
     public void testAsSAU() throws AWTException, InterruptedException {
+        testConfig().getTestRail().setCaseID(82782);
+        testConfig().getTestRail().setTester("Denys_G");
         setUpUser(User.UserType.SAU); //pass userType and browser. see ~/QAUberTestConfig
-
         setUpScript();
-
+        assignPermissionsEntities();
 
         //flowing method sets precondition for AU in next test(while already logged in as SAU)
-        assignPermissionsEntities();
         getPreconditions().getPermissionsPreconditions().setAUasAdminToAllEntities(testDriver());
-        breakDownHelper();
+        Thread.sleep(sleepTime/4);
+
+        breakDownHelper();//force close browser for this test while @afterTest doesn't:(
     }
     @Test(priority = 1)
     public void testAsAU() throws AWTException, InterruptedException {
-//        getPreconditions().getPermissionsPreconditions().ensureAUIsAdminAllEntities();
 
+//  This precondition isn't used to save time of opening new browser(log in as SAU and verify preconditions)
+//  set precondition(which is part of ensure precondition) is used in test above
+//  getPreconditions().getPermissionsPreconditions().ensureAUIsAdminAllEntities();
+
+        testConfig().getTestRail().setCaseID(82784);
+        testConfig().getTestRail().setTester("Denys_G");
         setUpUser(User.UserType.AU); //pass userType and browser. see ~/QAUberTestConfig
 
         setUpScript();
@@ -54,7 +60,7 @@ public class EntitiesAssignPermissions extends PageObjectModelResources {
     public void assignPermissionsEntities() throws InterruptedException, AWTException {
 
         /** NOTE: index in the list is different while logged in as different user(different permissions)*/
-        int EntityDepSubdepIndex = 1;
+        int entityIndex = 1;
         int userIndex = 2;
 
         testDriver().get(testConfig().getBaseURL());
@@ -63,14 +69,27 @@ public class EntitiesAssignPermissions extends PageObjectModelResources {
         Thread.sleep(sleepTime);
         getNavBar().entitiesButton().click();
         Thread.sleep(sleepTime/2);
-        getEntities().assignPermissionButton(EntityDepSubdepIndex).click();
+        getEntities().assignPermissionButton(entityIndex).click();
         Thread.sleep(sleepTime/8);
         getEntitiesPermissionsDialog().detachUserPermissionsTR(userIndex);
         Thread.sleep(sleepTime/10);
-        getEntitiesPermissionsDialog().setUserAsRegularUser(userIndex);
+        getEntitiesPermissionsDialog().setUserAsRegularUser(userIndex);      //set as Regular user here
         Thread.sleep(sleepTime/10);
         getEntitiesPermissionsDialog().closeDialogByPressESC();
+        Thread.sleep(sleepTime/8);
+        getEntities().assignPermissionButton(entityIndex).click();
+        Thread.sleep(sleepTime/8);
+        try {
+            Assert.assertEquals(getEntitiesPermissionsDialog().rightsDropdownByIndex(userIndex)
+                    .getFirstSelectedOption().getText(), "Regular user"); //method setUserAsRegularUser
+        } catch (AssertionError e) {
+            testConfig().getTestRail().addResults(TestRail.TestCaseResult.FAILED, "Other permission shown "+e.getLocalizedMessage());
+            throw e;
+        }
         Thread.sleep(sleepTime/10);
+        getEntitiesPermissionsDialog().closeDialogByPressESC();
+        testConfig().getTestRail().addResults(TestRail.TestCaseResult.PASSED, "Test passed");
+
     }
 
     @AfterTest
