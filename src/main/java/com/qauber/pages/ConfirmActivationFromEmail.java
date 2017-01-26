8f1aa4d795d676318a5c46b568ee5cdf1dd193ec
erikfriedlander <friedlander.erik@gmail.com>
2017-01-25 18:54:06 -0800

    add: ConfirmActivationFromEmail ConfirmRegistrationFromEmail
    pages linked in email to activate AU / RU

---
+++ src/main/java/com/qauber/pages/ConfirmActivationFromEmail.java
@@ -0,0 +1,15 @@
+package com.qauber.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by erikfriedlander on 1/18/17.
+ */
+public class ConfirmActivationFromEmail {
+    WebDriver driver;
+    public ConfirmActivationFromEmail(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
